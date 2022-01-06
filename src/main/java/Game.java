e950d9eb3d2c9a400192884738749267ea55c88f
MoreiraP12 <up201905429@edu.fe.up.pt>
2022-01-06 11:04:30 +0000

    Game Test initiated

---
+++ src/main/java/Game.java
@@ -0,0 +1,18 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    //private Arena arena;
+    private Screen screen;
+    public boolean gameover;
+    public Game() throws IOException {}
+
+    private void draw(){}
+
+    public void run(){}
+}
---
+++ src/test/groovy/GameTest.groovy
@@ -0,0 +1,18 @@
+import com.googlecode.lanterna.graphics.TextGraphics
+import spock.lang.Specification
+
+class GameTest {
+    private Game game
+
+
+    def setup(){
+
+
+    }
+
+    def"Constructor test"(){}
+
+    def"Draw test"(){}
+
+    def"Run test"(){}
+}
