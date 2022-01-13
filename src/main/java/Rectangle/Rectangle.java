0f971b938f097e4f552db358578f71a7b552069d
DanielRodrigues202006562 <up202006562@up.pt>
2022-01-13 23:53:42 +0000

    Code Rectangle

--- src/main/java/Rectangle/Rectangle.java
+++ src/main/java/Rectangle/Rectangle.java
@@ -4,31 +4,49 @@ public class Rectangle {
     private Position position;
     private Dimension dimension;
 
-    public Rectangle(Position position, Dimension dimension){}
+    public Rectangle(Position position, Dimension dimension){
+        this.position = position;
+        this.dimension = dimension;
+    }
 
-    public boolean intersect(Rectangle r1){return false;}
+    public boolean intersect(Rectangle otherRectangle){
+        Position l1 = new Position(getX(),getY());
+        Position r1 = new Position(getX()+getWidth(),getY()+getHeight());
+        Position l2 = new Position(otherRectangle.getX(), otherRectangle.getY());
+        Position r2 = new Position(otherRectangle.getX() + otherRectangle.getWidth(), otherRectangle.getY()+otherRectangle.getHeight());
+
+        if(l1.getY()>r2.getY())
+            return false;
+        if(r1.getY() < l2.getY())
+            return false;
+        if(l1.getX()>r2.getX())
+            return false;
+        if(r1.getX()<l2.getX())
+            return false;
+        return true;
+    }
 
     public int getX() {
-        return 0;
+        return position.getX();
     }
 
     public int getY() {
-        return 0;
+        return position.getY();
     }
 
-    public void setX(int x){}
+    public void setX(int x){position.setX(x);}
 
-    public void setY(int y){}
+    public void setY(int y){position.setY(y);}
 
-    public void updateX(int speed){}
+    public void updateX(int speed){position.updateX(speed);}
 
-    public void updateY(int speed){}
+    public void updateY(int speed){position.updateY(speed);}
 
-    public int getHeight(){return 0;}
+    public int getHeight(){return dimension.getHeight();}
 
-    public int getWidth(){return 0;}
+    public int getWidth(){return dimension.getWidth();}
 
-    public void setHeight(int height){}
+    public void setHeight(int height){dimension.setHeight(height);}
 
-    public void setWidth(int width){}
+    public void setWidth(int width){dimension.setWidth(width);}
 }
--- src/test/groovy/RectangleTest.groovy
+++ src/test/groovy/RectangleTest.groovy
@@ -5,54 +5,39 @@ class RectangleTest extends Specification{
     private Rectangle rectangle
 
     void setup(){
-        rectangle = new Rectangle(new Position(20,0), new Dimension(10,10))
+        rectangle = new Rectangle(new Position(30,30), new Dimension(10,10))
     }
 
     def"Test Update Y"(){
         when:
             rectangle.updateY(10)
         then:
-            rectangle.getY() == 20
+            rectangle.getY() == 40
     }
 
     def"Test Update  X"(){
         when:
             rectangle.updateX(10)
         then:
-            rectangle.getX() == 0
+            rectangle.getX() == 20
     }
 
-    def"Test intersect(Top)"(){
+    def"Test intersect"(){
         expect:
-        rectangle.intersect(new RectangleTest(pos, dimension))
+        rectangle.intersect(new Rectangle(pos, dimension)) == bool
+
 
         where:
         pos | dimension | bool
         new Position(18,0) | new Dimension(1,1) |false
-        new Position(31,0) | new Dimension(1,1) |false
-        new Position(25,11) | new Dimension(1,1) |false
-        new Position(19,0) | new Dimension(1,1) |true
-        new Position(29,0) | new Dimension(1,1) |true
-        new Position(19,10) | new Dimension(1,1) |true
-        new Position(29,10) | new Dimension(1,1) |true
-        new Position(25,5) | new Dimension(1,1) | true
-    }
-    def"Test intersect(Bottom)"(){
-        given:
-        Rectangle r1 = new Rectangle(new Position(20,30), new Dimension(10,10))
-        expect:
-        r1.intersect(new RectangleTest(pos, dimension))
+        new Position(40,0) | new Dimension(1,1) |false
+        new Position(25,35) | new Dimension(1,1) |false
+        new Position(43,35) | new Dimension(1,1) |false
 
-        where:
-        pos | dimension | bool
-        new Position(19,0) | new Dimension(1,1) | false
-        new Position(20,15) | new Dimension(1,1) |false
-        new Position(31,39) | new Dimension(1,1) | false
-        new Position(19,39) | new Dimension(1,1) |true
-        new Position(30,39) | new Dimension(1,1) |true
-        new Position(19,29) | new Dimension(1,1) |true
+        new Position(29,35) | new Dimension(1,1) |true
+        new Position(40,35) | new Dimension(1,1) |true
         new Position(30,29) | new Dimension(1,1) |true
-        new Position(25,35) | new Dimension(1,1) |true
+        new Position(32,40) | new Dimension(1,1) | true
     }
 
 }
