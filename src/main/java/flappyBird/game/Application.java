bf6ee522a445e668afecd0e4a7c3970b6f7dc372
DanielRodrigues202006562 <up202006562@up.pt>
2022-01-15 16:32:22 +0000

    Add Design Pattern - Singleton

--- src/main/java/flappyBird/game/Application.java
+++ src/main/java/flappyBird/game/Application.java
@@ -1,19 +1,20 @@
 package flappyBird.game;
 
 import flappyBird.menus.Gui;
-
 import java.io.IOException;
 
 public class Application {
+    private static Game singleton = null;
+
+    public static Game getInstance() throws IOException{
+        if(singleton == null)
+            singleton = new Game(new Gui());
+        return singleton;
+    }
 
 
-
-    public static void main(String[] args) throws IOException {
-        Game game = new Game(new Gui());
-        try {
-            game.start();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws Throwable {
+        Game game = getInstance();
+        game.start();
     }
 }
