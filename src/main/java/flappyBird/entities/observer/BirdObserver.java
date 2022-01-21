43f50896791623b91b908e343edae3540feecf0f
DanielRodrigues202006562 <up202006562@up.pt>
2022-01-21 14:39:12 +0000

    more bug fix

--- src/main/java/flappyBird/entities/observer/BirdObserver.java
+++ src/main/java/flappyBird/entities/observer/BirdObserver.java
@@ -1,13 +1,16 @@
 package flappyBird.entities.observer;
 
+import flappyBird.MusicPlayer;
 import flappyBird.entities.Bird;
 import flappyBird.entities.Entities;
 
 public class BirdObserver implements EntitiesObserver{
     private int height;
+    private MusicPlayer collideSound;
 
     public BirdObserver(int height){
         this.height = height;
+        collideSound = new MusicPlayer("collide.wav");
     }
 
 
@@ -19,6 +22,7 @@ public class BirdObserver implements EntitiesObserver{
             if(entity instanceof Bird){
                 ((Bird) entity).decreaseLives(1);
             }
+            collideSound.playSound();
             entity.setY(height/2);
         }
     }
